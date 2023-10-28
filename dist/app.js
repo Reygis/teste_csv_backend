31b0f115076cf634c44debfe6ad1c6cb15b93645
Reygis Azevedo <reygis@outlook.com>
2023-10-28 19:30:01 -0300

    add body-parser for solve versel problems

--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,7 @@ const express_fileupload_1 = __importDefault(require("express-fileupload"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use((0, express_fileupload_1.default)({ createParentPath: true }));
 app.use((0, cors_1.default)());
 app.use(routes_1.default);
--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import bodyParser from 'body-parser'
 
 const app = express()
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload({ createParentPath: true }))
 app.use(cors())
 app.use(routes)
